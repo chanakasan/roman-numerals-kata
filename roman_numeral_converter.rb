63be84a479cc2c0949134f1afa0d3e5b3f0d4e53
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-07-16 21:19:22 +0530

    add example for 9

--- roman_numeral_converter.rb
+++ roman_numeral_converter.rb
@@ -1,6 +1,14 @@
 class RomanNumeralConerter
   def convert(number)
     result = ""
+    if number == 4
+      result << "IV"
+      number -= 4
+    end
+    if number == 9
+      result << "IX"
+      number -= 9
+    end
     if number >= 10
       result << "X"
       number -= 10
@@ -8,10 +16,6 @@ class RomanNumeralConerter
     if number >= 5
       result << "V"
       number -= 5
-    end
-    if number == 4
-      result << "IV"
-      number -= 4
     end
     result << "I" * number
     result
--- roman_numeral_converter_spec.rb
+++ roman_numeral_converter_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe RomanNumeralConerter do
   Then { roman_numeral_converter.convert(6) == "VI" }
   Then { roman_numeral_converter.convert(7) == "VII" }
   Then { roman_numeral_converter.convert(8) == "VIII" }
+  Then { roman_numeral_converter.convert(9) == "IX" }
   Then { roman_numeral_converter.convert(10) == "X" }
 end
