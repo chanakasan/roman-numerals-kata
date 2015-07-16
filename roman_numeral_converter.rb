06de8eb49376ef35bfd94e3e38155bc27e5eff64
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-07-16 21:26:01 +0530

    refactor part1

--- roman_numeral_converter.rb
+++ roman_numeral_converter.rb
@@ -1,23 +1,26 @@
 class RomanNumeralConerter
   def convert(number)
     result = ""
-    if number == 4
+    while number == 4
       result << "IV"
       number -= 4
     end
-    if number == 9
+    while number == 9
       result << "IX"
       number -= 9
     end
-    if number >= 10
+    while number >= 10
       result << "X"
       number -= 10
     end
-    if number >= 5
+    while number >= 5
       result << "V"
       number -= 5
     end
-    result << "I" * number
+    while number >= 1
+      result << "I"
+      number -= 1
+    end
     result
   end
 end
