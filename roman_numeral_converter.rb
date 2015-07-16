4154472334353b64c576b7075bf497e9f59956fa
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-07-16 21:35:49 +0530

    refactor part3

--- roman_numeral_converter.rb
+++ roman_numeral_converter.rb
@@ -1,20 +1,14 @@
 class RomanNumeralConerter
   ROMAN_SYMBOLS = [
     [10, "X"],
+    [9, "IX"],
     [5, "V"],
+    [4, "IV"],
     [1, "I"],
   ]
 
   def convert(number)
     result = ""
-    if number == 4
-      result << "IV"
-      number -= 4
-    end
-    if number == 9
-      result << "IX"
-      number -= 9
-    end
     ROMAN_SYMBOLS.each do |limit, symbol|
       while number >= limit
         result << symbol
